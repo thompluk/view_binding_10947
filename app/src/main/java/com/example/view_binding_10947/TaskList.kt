6b709a9e8aec592c3e401a553ff29d8309357a34
thompluk <thomasdany38@gmail.com>
2022-09-13 15:09:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10947/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.view_binding_10947
+
+object TaskList {
+
+    val taskList= listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari-hari untuk membuat hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha  Mama Yaemiko tercinta / Keqing wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mana Ei?", "Buat Adu Mekanik Showcase Ayaka Pyro"),
+        Task("Chapter 5", "Upload ke Tiktok", "Buat PAmer ke view Showcase Ayaka Pyro C6 + Bennet C6")
+        )
+
+}
